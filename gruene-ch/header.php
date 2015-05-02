c0456f67d9b83c17c99aab93dc6a1d8016425cdd
politbuero-kampagnen <balthasar.glaettli@politbuero-kampagnen.ch>
2015-05-02 18:25:41 +0200

    Added Child-Theme-Directory with Files

---
+++ gruene-ch/header.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package Forefront
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+	<?php do_action( 'before' ); ?>
+	<header id="masthead" class="site-header" role="banner">
+		<div class="site-branding">
+			<?php $header_image = get_header_image();
+			if ( ! empty( $header_image ) ) { ?>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" class="header-image-link">
+					<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" />
+				</a>
+			<?php } ?>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+		</div>
+
+		<nav id="site-navigation" class="navigation-main" role="navigation">
+			<h1 class="menu-toggle"><?php _e( 'Menu', 'forefront' ); ?></h1>
+
+			<a href="#content" class="screen-reader-text skip-link"><?php _e( 'Skip to content', 'forefront' ); ?></a>
+
+			<?php wp_nav_menu( array( 
+				'theme_location' => 'primary',
+				'menu'            => '',
+				'container'       => 'div',
+				'container_class' => '',
+				'container_id'    => 'nav-global-box',
+				'menu_class'      => 'menu',
+				'menu_id'         => '',
+				'echo'            => true,
+				'fallback_cb'     => 'wp_page_menu',
+				'before'          => '',
+				'after'           => '',
+				'link_before'     => '',
+				'link_after'      => '',
+				'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+				'depth'           => 0,
+				'walker'          => '')) 
+			?>
+		</nav>
+	</header><!-- #masthead -->
+
+	<div id="main" class="site-main">
---
+++ gruene-ch/page-templates/front-page.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Template Name: Front Page
+ *
+ * @package Forefront
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area front-page-content-area full-width">
+		<div id="content" class="site-content" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+			<div class="hero <?php echo forefront_additional_class(); ?>">
+				<?php if ( '' != get_the_post_thumbnail() ) : ?>
+				<figure>
+					<?php the_post_thumbnail( 'forefront-page-thumbnail' ); ?>
+					<div>
+						<div>
+							<div>
+								<h1 class="page-title"><?php the_title(); ?></h1>
+							</div>
+						</div>
+					</div>
+				</figure>
+				<?php endif; ?>
+				<div>
+					<div>
+						<div>
+							<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+								<header class="entry-header">
+									<h1 class="page-title sanukot"><?php the_title(); ?></h1>
+								</header>
+								<div class="slogan">
+								<div class="title-bar-green"><span class="leerzeichen sanukot">Titel Green</span></div>
+								<div class="title-bar-magenta"><span class="leerzeichen sanukot">Titel Magenta</span></div></div>
+								<div class="entry-content">
+									<?php the_content(); ?>
+								</div>
+								<?php edit_post_link( __( 'Edit', 'forefront' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
+							</article>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+	<?php get_sidebar( 'front-page-one' ); ?>
+
+	<?php get_sidebar( 'front-page-two' ); ?>
+
+	<?php $testimonials = forefront_get_random_posts( 2, 'jetpack-testimonial' ); ?>
+
+	<?php if ( ! empty( $testimonials ) ) : ?>
+	<div class="clear testimonials">
+		<?php
+			foreach ( $testimonials as $testimonial ) : setup_postdata( $GLOBALS['post'] =& $testimonial );
+				 get_template_part( 'content', 'testimonial' );
+			endforeach;
+			wp_reset_postdata();
+		?>
+	</div>
+	<?php endif; ?>
+
+<?php get_footer(); ?>
